aec0b4525d683bfb9433924e123e7b13a07c4d82
ziqichen <zqchen6787@gmail.com>
2020-05-15 22:35:06 -0400

    Created a new repo for Spring Data JPA

---
+++ src/main/java/com/luv2code/cruddemo/service/EmployeeServiceImpl.java
@@ -0,0 +1,45 @@
+package com.luv2code.cruddemo.service;
+
+import com.luv2code.cruddemo.dao.EmployeeDAO;
+import com.luv2code.cruddemo.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private EmployeeDAO employeeDAO;
+
+    @Autowired
+    public EmployeeServiceImpl(@Qualifier("employeeDAOJpaImpl") EmployeeDAO employeeDAO) {
+        this.employeeDAO = employeeDAO;
+    }
+
+    @Override
+    @Transactional
+    public List<Employee> findAll() {
+        return employeeDAO.findAll();
+    }
+
+    @Override
+    @Transactional
+    public Employee findById(int theId) {
+        return employeeDAO.findById(theId);
+    }
+
+    @Override
+    @Transactional
+    public void save(Employee theEmployee) {
+        employeeDAO.save(theEmployee);
+    }
+
+    @Override
+    @Transactional
+    public void deleteById(int theId) {
+        employeeDAO.deleteById(theId);
+    }
+}
